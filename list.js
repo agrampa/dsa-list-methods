2ccd434eabd5abcb5103ac51ddde995f8cfebfeb
Allie Grampa <agrampa@yahoo.com>
2017-04-26 09:12:07 -0700

    finished filter method

---
+++ list.js
@@ -0,0 +1,52 @@
+'use strict';
+
+function List() {
+  for(let keys in arguments) {
+    this[key] = arguments[key];
+  }
+  this.length = arguments.length;
+}
+
+List.prototype.copy = function() {
+  let result = new List();
+  for(let key in this) {
+    result[key] = this[key];
+  }
+  return result;
+}
+
+List.prototype.push = function(value) {
+  let result = this.copy();
+  result[resul.length++] = value;
+  return result;
+}
+
+List.prototype.pop = function() {
+  let result = this.copy();
+  delete result[--result.length];
+  return{
+    value: this[this.length - 1],
+    list: result;
+  }
+}
+
+List.prototype.forEach = function(callback) {
+  for(let i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+  }
+}
+
+List.prototype.filter = function(callback) {
+  let result = [];
+  for(let i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+    if (i === true) result.push[i];
+  }
+  return results;
+}
+
+List.prototype.map = function(callback) {
+  let result = [];
+}
+
+List.prototype.reduce = function() {}
